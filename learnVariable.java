f766d99563a2207dcfed01b7d19809241ec21f13
Aliesy1628 <gulaku1628@gmail.com>
2025-10-23 14:12:38 +0700

    Tambah Honkai

--- learnVariable.java
+++ learnVariable.java
@@ -1,5 +1,6 @@
 public class learnVariable {
         public static void main(String[] args) {
             System.out.println("Hello World");
+            System.out.println("Honkai");
         }
 }

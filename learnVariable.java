4a73226696284f6df05a8f0b75901ae199917b4d
Aliesy1628 <gulaku1628@gmail.com>
2025-10-23 14:14:04 +0700

    Honkai3

--- learnVariable.java
+++ learnVariable.java
@@ -2,5 +2,7 @@ public class learnVariable {
         public static void main(String[] args) {
             System.out.println("Hello World");
             System.out.println("Honkai");
+            System.out.println("Honkai2");
+            System.out.println("Honkai3");            
         }
 }
